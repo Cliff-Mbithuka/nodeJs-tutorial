242d706f76b53a0c59843b0351dde553bbae5eaa
Clifford Mbithuka <cliffordmusyoka1@gmail.com>
2024-04-24 14:44:41 +0300

    Add files

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -6,6 +6,12 @@ const fileOps = async () => {
     try{
 const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
 console.log(data);
+await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt')); //used to delete
+await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\n Nice to meet you');
+await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+console.log(newData);
     }catch (err) {
     console.error(err);
     }
