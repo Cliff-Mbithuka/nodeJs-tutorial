8c741fa3e2ae9b5e1b58b70b9ce1db31a2262e91
Clifford Mbithuka <cliffordmusyoka1@gmail.com>
2024-04-26 11:05:36 +0300

    Add files

--- routing/routes/api/employees.js
+++ routing/routes/api/employees.js
@@ -11,21 +11,21 @@ router.route('/')
     res.json({
 "firstname": req.body.firstname, 
 "lastname": req.body.lastname
-    })
+    });
 })
 .put((req, res) => {
     res.json({
         "firstname": req.body.firstname, 
         "lastname": req.body.lastname
-    })
+    });
 })
 .delete((req, res) => {
     res.json({"id": req.body.id})
 });
 
-router.route('/id')
+router.route('/:id')
 .get((req, res) => {
     res.json({ "id": req.params.id });
 });
 
-module.imports = router;
+module.exports = router;
